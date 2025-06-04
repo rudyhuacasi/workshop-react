daadb005db01109c4be7afb8739e5b4db8e00f32
rudyhuacasi <alexisitaliano16@gmail.com>
2025-06-04 11:03:54 +0200

    {UPD} added install chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Heading, Stack, Button, Container } from "@chakra-ui/react"
+import { DecorativeBox } from "compositions/lib/decorative-box"
+import * as API from "./services/launches";
 import './App.css'
 
 function App() {
-  
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches)
+  }, []);
 
   return (
     <>
+    <div style={{ padding: 20 }}>
+      <Button colorScheme="teal">¡Hola Chakra!</Button>
+    </div>
+    <Container>
+      <Heading as="h4" size="5xl">Space X</Heading>
+      <ul>
+        {launches.map(launch => (
+          <DecorativeBox key={launch.flight_number}>
+            {launch.mission_name} ({launch.launch_year})
+          </DecorativeBox>
+        ))}
+      </ul>
+    </Container>
+      
     </>
   )
 }
